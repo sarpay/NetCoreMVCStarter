87bff17c1c4e8f555716431258353d691836aa5c
Sarpay Öner <sarpay@gmail.com>
2019-03-11 13:58:33 +0300

    first commit

---
+++ NetCoreMVC/Models/HomeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreMVC.Models
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+    }
+
+    public class CategoryDTO
+    {
+        public string Name { get; set; }
+    }
+    public class HomeViewModel
+    {
+        public CategoryDTO Category { get; set; }
+        public List<ProductDTO> Products { get; set; }
+
+    }
+}
